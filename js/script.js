27185f0685813f090e3b370c8d6e8fb32fd8eb2f
MASTERDOSNOOBS <jairderioboeing@gmail.com>
2024-03-14 22:34:52 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-let list = () => {
-    fetch('https://restcountries.com/v3.1/all')
+let list = (bySearch) => {
+    let url = 'https://restcountries.com/v3.1/all';
+    let searchValue = document.getElementById('search').value;
+    
+    if (bySearch && searchValue) {
+        url = `https://restcountries.com/v3.1/name/${searchValue}`;
+    }
+    fetch(url)
         .then(json => json.json())
         .then(countries => {
             let row = document.querySelector('#row')
@@ -22,86 +28,4 @@ let list = () => {
             row.innerHTML = countriesHtml;
         });
 }
-
-list();  
-
-let row = document.querySelector('#row');
-let form = document.querySelector('.form');   
-let nameC = document.querySelector('#search').value;
-let countriesHtml = "";
-
-form.addEventListener('submit', (event) =>{
-    console.log("aa");
-});
-let search = () =>{
-    fetch('https://restcountries.com/v3.1/all').then(json => json.json())
-        .then(countries => {
-           
-            for (let index = 0; index < countries.length; index++) {
-                const country = countries[index];
-                if (nameC === country.translations.por.common) {
-                    countriesHtml += `
-                        <div class="col-4 mt-5">
-                            <div class="card" style="width: 18rem;">
-                                <img src="${country.flags.png}" class="card-img-top" alt="${country.flags.alt}">
-                                <div class="card-body">
-                                <h5 class="card-title">${country.translations.por.common}</h5>
-                                <p class="card-text">Região: ${country.region}</p>
-                                </div>
-                            </div>
-                        </div>`;
-                    row.innerHTML = countriesHtml;
-                    return
-                }
-                row.innerHTML = "Not found";
-                
-
-               
-            }
-
-        }
-    );
-}
-
-
-
-// function buscaPaises(param) {
-//     var url = "https://restcountries.com/v3.1/all";
-
-//     var busca = document.getElementById('busca').value;
-//     if(param && busca){
-//         url = "https://restcountries.com/v3.1/name/"+busca
-//     }
-
-//     fetch(url).then(data => {
-//         return data.json();
-//     }).then(paises => {
-//         console.log(paises);
-
-//         var html_final = '';
-
-//         for (let i = 0; i < paises.length; i++) {
-//             //aqui a gente tem pais por pais
-//             const pais = paises[i];
-
-//             //aqui utilizamos crase ``
-//             var html_base = `
-//             <div class="col-4">
-//                 <div class="card shadow mb-4" style="width: 18rem;">
-//                     <img style="height: 180px; object-fit: cover;" src="${pais.flags.png}" class="card-img-top" alt="...">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${pais.name.common}</h5>
-//                         <p class="card-text">Regiao: ${pais.region}</p>
-//                     </div>
-//                 </div>
-//             </div>
-//             `;
-
-//             html_final = html_final + html_base;            
-//         }
-
-//         document.getElementById('linha').innerHTML = html_final;
-//     })
-// }
-
-// buscaPaises();
+list();
